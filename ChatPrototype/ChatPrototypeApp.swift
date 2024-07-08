5fd4dd5f8664cd52761d029c92991a063deb7eb8
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-08 13:37:44 -0400

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Jordan Haynes on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
